6d0e1b434c0e82b12b8c2d99c7b7f7872c7844b5
ariel klevecz <ariel@yaytso.art>
2024-09-11 23:08:20 -0700

    map

--- mapToComponent.cjs
+++ mapToComponent.cjs
@@ -16,14 +16,50 @@ fs.readFile(svgFilePath, "utf8", (err, data) => {
   const svgContent = data.replace(/<\?xml.*?\?>/, "").replace('<svg ', '<svg {style} ').replace(/ isolation="isolate"/g, '').replace(/text-orientation="sideways-right" /g,'').trim();
 
   // Wrap the SVG content in a Svelte component template
-  const svelteComponentContent = `<script>
+  const svelteComponentContent = `
+  <script>
+  import { browser } from "$app/environment";
+  import data from "$lib/data";
+  import { onMount } from "svelte";
+  import Modal from "./modal.svelte";
+  import { goto } from "$app/navigation";
+
   /** @type {{style:string}} props */
   let { style } = $props();
-</script>
 
-<style>
-    /* Add any necessary styles here */
-</style>
+  let showDoNotEnter = $state(false);
+  onMount(() => {
+    if (browser) {
+      const allRecs = document.querySelectorAll("rect");
+      const unitRecs = Array.from(allRecs).filter((rec) => rec.id.includes("unit"));
+
+      for (const unitRec of unitRecs) {
+        const unitNumber = unitRec.id.replace("unit_x5F_", "");
+        const artist = data.find((artist) => artist.unit === unitNumber);
+
+        if (artist) {
+          unitRec.style.fill = "green";
+          unitRec.style.opacity = "0.25";
+        } else {
+          unitRec.style.fill = "black";
+          unitRec.style.opacity = "0.75";
+        }
+
+        unitRec.addEventListener("click", () => {
+          if (artist) {
+            const link = "/"+artist.name;
+            goto(link);
+          } else {
+            showDoNotEnter = true;
+          }
+        });
+      }
+    }
+  });
+</script>
+<Modal bind:showModal={showDoNotEnter} title="DO NOT ENTER">
+  <p>Sorry, this is a private studio. Please do not enter.</p>
+</Modal>
 
 ${svgContent}`;
 
