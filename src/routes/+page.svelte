6f0df7e779209466a09db19fa31a4a364cabcd4d
ariel klevecz <ariel@yaytso.art>
2024-10-14 22:43:57 -0700

    improve desk

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,25 +10,22 @@
 <svelte:head>
   <title>Lacy Studios</title>
   <meta property="og:title" content="Lacy Studios" />
-  <meta
-    property="og:description"
-    content="Lacy Studio Lofts in Los Angeles, CA. 2684 Lacy St. 90031"
-  />
+  <meta property="og:description" content="Lacy Studio Lofts in Los Angeles, CA. 2684 Lacy St. 90031" />
   <meta property="og:image" content={"/seo-img.png"} />
 </svelte:head>
 <div
   class={orientationSvelte.state}
-  style="display:flex;min-height:{orientationSvelte.dimensions.height}px;padding:1rem;"
+  style="display:flex;flex-wrap:wrap;min-height:{orientationSvelte.dimensions.height}px;padding:1rem;"
 >
-  <div class="before" style="flex: 0 1;">
-    <h1>LACY STUDIOS</h1>
-  </div>
-  <div class="map" style="display:flex;justify-content:center;">
-    <LacyMap style="max-width:100%;height:90%;" />
+<div class="before" style="flex: 1 0 100%;">
+  <h1>LACY STUDIOS</h1>
+</div>
+  <div class="map" style="display:flex;flex-direction:column;align-items:center;">
+
+    <LacyMap style="" />
   </div>
   <div class="after" style="">
-    <nav style="display:flex;margin:1rem .5rem;gap:1rem;">
-    </nav>
+    <nav style="display:flex; margin:1rem .5rem; gap:1rem;"></nav>
     {#if view === "artists"}
       <ArtistList />
     {/if}
@@ -47,20 +44,31 @@
   .portrait > div {
     flex: 1 0 33%;
   }
-  .map {
-    height: 300px;
-  }
+
   .landscape .map {
     height: 90vh;
   }
-  .landscape > .before {
-    display: none;
+
+  @media (max-width: 768px) {
+    .landscape .before {
+      display: none;
+    }
+    .landscape > .after {
+      display: none;
+    }
   }
-  .landscape > .after {
-    display: none;
+
+  @media (min-width: 768px) {
+    .after {
+      padding: 0 4rem;
+    }
   }
+
   .map {
     width: 100%;
+    max-width: 768px;
+    height: 300px;
+
   }
   .landscape {
     flex-direction: row;
